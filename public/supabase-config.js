39082f4de0b8b8d8e7083516d6e2809f421ade21
pablogesell1979 <gaiazzipablo@gmail.com>
2026-07-26 18:07:58 -0300

    Add files via upload

---
+++ public/supabase-config.js
@@ -0,0 +1,5 @@
+// supabase-config.js
+const SUPABASE_URL = 'https://jymjdemtadolahcdvnxq.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5bWpkZW10YWRvbGFoY2R2bnhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwODk1MjQsImV4cCI6MjEwMDY2NTUyNH0.AdXrhafJz5C4e0yapTPZfUoJqiQNcZ_4fbsVehhqQjo';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
