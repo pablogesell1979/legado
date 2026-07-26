48643f39c9806d7f0a0f9d962d341a6541210956
pablogesell1979 <gaiazzipablo@gmail.com>
2026-07-26 18:21:37 -0300

    Add files via upload

--- public/supabase-config.js
+++ public/supabase-config.js
@@ -2,5 +2,8 @@
 const SUPABASE_URL = 'https://jymjdemtadolahcdvnxq.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5bWpkZW10YWRvbGFoY2R2bnhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwODk1MjQsImV4cCI6MjEwMDY2NTUyNH0.AdXrhafJz5C4e0yapTPZfUoJqiQNcZ_4fbsVehhqQjo';
 
-// Le cambiamos el nombre a "clienteSupabase" para evitar conflictos
-const clienteSupabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Lo guardamos en 'window' para que sea global y 100% visible para todo el juego
+window.clienteSupabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// Agregamos este mensaje para ver en la consola si funcionó
+console.log("✅ Cliente Supabase cargado:", window.clienteSupabase);
